14363e9612314940f018973cbdf6c5e1754b5bd0
Niklas Mårdby <niklas.mardby@gmail.com>
2024-03-29 00:19:49 +0100

    Conditional rendering of pizza orders

--- src/components/PizzaOrder/PizzaOrder.tsx
+++ src/components/PizzaOrder/PizzaOrder.tsx
@@ -1,11 +1,20 @@
+import { useContext } from "react";
 import Button from "../Button/Button";
 import "./PizzaOrder.scss";
+import { GlobalStateContext } from "../../state/GlobalStateContext";
 
 const PizzaOrder = () => {
+	const { state } = useContext(GlobalStateContext);
+
 	return (
 		<section className="PizzaOrder">
 			<h2>Your Pizza Order</h2>
 			<p>[ här visar vi alla beställda pizzor ]</p>
+			{state.pizzaOrder.length > 0 ? (
+				<div>alla pizzor</div>
+			) : (
+				<div>Please add a pizza!</div>
+			)}
 			<Button
 				handleClick={() => {
 					console.log("hit?");
--- src/state/GlobalStateContext.tsx
+++ src/state/GlobalStateContext.tsx
@@ -1,6 +1,15 @@
 import { createContext } from "react";
 import { Action, GlobalState, Pizza } from "../types/GlobalState";
 
+/* const testPizza: Pizza = {
+	pizzaMenuOption: { name: "Test Pizza", cost: 5 },
+	extraToppings: [
+		{ name: "Test Pizza", cost: 5 },
+		{ name: "Test Pizza", cost: 5 },
+	],
+	size: { name: "Test Pizza", cost: 5 },
+}; */
+
 export const initialState = { pizzaOrder: [] as Pizza[] };
 
 export const GlobalStateContext = createContext<{
