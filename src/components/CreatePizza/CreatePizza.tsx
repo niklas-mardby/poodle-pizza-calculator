c3efa4380670feeb1a6d9ec8d205785b70d31208
Niklas Mårdby <niklas.mardby@gmail.com>
2024-03-28 15:13:31 +0100

    Small update to CreatePizza component

--- src/components/CreatePizza/CreatePizza.tsx
+++ src/components/CreatePizza/CreatePizza.tsx
@@ -1,7 +1,20 @@
+import Button from "../Button/Button";
 import "./CreatePizza.scss";
 
 const CreatePizza = () => {
-	return <section className="CreatePizza">CreatePizza</section>;
+	return (
+		<section className="CreatePizza">
+			<h2>Create Pizza</h2>
+			<p>[ create pizza form goes here]</p>
+			<Button
+				handleClick={() => {
+					console.log("hit?");
+				}}
+			>
+				Add Pizza
+			</Button>
+		</section>
+	);
 };
 
 export default CreatePizza;
