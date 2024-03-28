84a42f116fc4f750b1be1981d907078b6638b580
Niklas Mårdby <niklas.mardby@gmail.com>
2024-03-28 15:11:51 +0100

    Updated pizzaorder component and style

--- src/components/PizzaOrder/PizzaOrder.tsx
+++ src/components/PizzaOrder/PizzaOrder.tsx
@@ -1,7 +1,20 @@
+import Button from "../Button/Button";
 import "./PizzaOrder.scss";
 
 const PizzaOrder = () => {
-	return <section className="PizzaOrder">PizzaOrder</section>;
+	return (
+		<section className="PizzaOrder">
+			<h2>Your Pizza Order</h2>
+			<p>[ här visar vi alla beställda pizzor ]</p>
+			<Button
+				handleClick={() => {
+					console.log("hit?");
+				}}
+			>
+				Clear Order
+			</Button>
+		</section>
+	);
 };
 
 export default PizzaOrder;
